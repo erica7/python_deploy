d62dc89606cdafc407559ec4635331f3c7271f77
Erica <ericaesnider@gmail.com>
2017-03-23 15:30:16 -0500

    added django messages

--- apps/login_and_reg/views.py
+++ apps/login_and_reg/views.py
@@ -1,48 +1,48 @@
 from django.shortcuts import render, redirect
 from .models import User
+from django.contrib import messages
 
 # request.session:
-    # messages: any errors or feedback to users
+    # session_messages: any errors or feedback to users   - replacing with messges framework
     # logged_in: true or false
     # user: first name of logged in user
 
 
 def index(request):                                 # GET : RENDER
-    context = request.session
-    return render(request, 'index.html', context)
+    return render(request, 'login_and_reg/index.html')
 
 def register(request):                              # POST : REDIRECT
     results = User.objects.register(request.POST)
     if results[0] == False:
-        request.session['messages'] = results[1]
+        for each in results[1]:
+            messages.info(request, each)
         return redirect('/')
     elif results[0] == True:
         request.session['logged_in'] = True
         request.session['user'] = results[1]
         return redirect('/success')
     else:
-        request.session['messages'] = "something went wrong"
+        messages.info(request, "something went wrong")
         return redirect('/')
 
 def login(request):                                 # POST : REDIRECT
     results = User.objects.login(request.POST)
     if results[0] == False:
-        request.session['messages'] = results[1]
+        for each in results[1]:
+            messages.info(request, each)
         return redirect('/')
     elif results[0] == True:
         request.session['logged_in'] = True
         request.session['user'] = results[1]
         return redirect('/success')
     else:
-        request.session['messages'] = "something went wrong"
+        messages.info(request, 'something went wrong')
         return redirect('/')
 
 def success(request):                               # GET : RENDER
-    return render(request, 'success.html')
+    return render(request, 'login_and_reg/success.html')
 
 def logout(request):                                # POST : REDIRECT
     request.session.pop('user')
-    if 'messages' in request.session:
-        request.session.pop('messages')
     request.session['logged_in'] = False
     return redirect('/')
